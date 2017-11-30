01be90038a12935fbf80c6826c3b4844edd38e3d
Nikolay Denisov <hm47web@gmail.com>
2017-11-30 15:27:39 +0400

    Fibonacci Numbers

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fibonacci', function () {
+    return view('fibonacci');
+});
+
+Route::get('fibonacci/{numbers}', 'FibonacciController@getNumbers');
+
